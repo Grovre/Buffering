9ab0792253610d5098cb699530879bd50556e074
grovre <landonvee111@gmail.com>
2023-11-18 23:44:14 -0600

    Better explanation of swap effects

--- Buffering/DoubleBuffering/DoubleBufferSwapEffect.cs
+++ Buffering/DoubleBuffering/DoubleBufferSwapEffect.cs
@@ -5,12 +5,15 @@ public enum DoubleBufferSwapEffect
     /// <summary>
     /// Flips the references to the resources in a double buffer.
     /// Should be used in most cases.
-    /// Back buffer will receive what was in the front and can either be discarded or used for the next resource update unless the front buffer was using the initial resource object from initialization
+    /// Back buffer will receive what was in the front buffer. This can either be discarded or used for the next
+    /// resource update unless the front buffer was using the initial resource object from initialization.
     /// </summary>
     Flip,
     /// <summary>
-    /// Copies the back buffer resource object to the front buffer resource object.
-    /// Generally barely slower than flipping and not needed in most cases
+    /// Copies the back buffer resource to the front buffer resource.
+    /// Generally slower than flipping, but not by a substantial amount unless the resource is large.
+    /// Flip is best-suited for most cases but copy is best-suited for when
+    /// the back buffer needs to know what is in the front buffer.
     /// </summary>
     Copy,
 }
