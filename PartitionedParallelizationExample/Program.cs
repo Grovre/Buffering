16e9780433d2322c2d36f910c2935557a7a950fb
Grovre <50428844+Grovre@users.noreply.github.com>
2023-12-17 11:29:40 -0600

    Create example parallel partition project

---
+++ PartitionedParallelizationExample/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Buffering.Parallel;
+
+Console.WriteLine("Allocating/Initializing");
+var data = new IntData[100_000_000];
+
+Console.WriteLine("Parallelization");
+var pp = new PartitionParallelizer<IntData>(
+    chunks: 8,
+    threadCount: 8,
+    data,
+    (ref IntData d) => d.N = 69);
+
+Console.WriteLine("Working");
+pp.Start();
+pp.Join();
+Console.WriteLine($"All data is 69?: {Array.TrueForAll(data, d => d.N == 69)}");
+
+record struct IntData(int N = -1);
