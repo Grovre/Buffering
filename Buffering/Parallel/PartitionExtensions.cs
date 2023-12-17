9d0d7841d9510ffd188172225f78d112ba72ba05
Grovre <50428844+Grovre@users.noreply.github.com>
2023-12-17 16:45:06 -0600

    Documentation for Parallel namespace

--- Buffering/Parallel/PartitionExtensions.cs
+++ Buffering/Parallel/PartitionExtensions.cs
@@ -2,8 +2,18 @@
 
 namespace Buffering.Parallel;
 
+/// <summary>
+/// Provides extensions for partitioning an array or span easily
+/// </summary>
 public static class PartitionExtensions
 {
+    /// <summary>
+    /// Outputs ranges representing a partitioned span
+    /// </summary>
+    /// <param name="span">The input span</param>
+    /// <param name="chunks">Amount of chunks/partitions</param>
+    /// <param name="dst">Where to output the ranges</param>
+    /// <typeparam name="T">Type of span</typeparam>
     public static void Partition<T>(this ReadOnlySpan<T> span, int chunks, Span<Range> dst)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(chunks);
@@ -23,9 +33,17 @@ public static class PartitionExtensions
         dst[chunks] = (chunks * chunkSize)..span.Length;
     }
 
+    /// <inheritdoc cref="Partition{T}(System.ReadOnlySpan{T},int,System.Span{System.Range})"/>
     public static void Partition<T>(this Span<T> span, int chunks, Span<Range> dst)
         => ((ReadOnlySpan<T>)span).Partition(chunks, dst);
 
+    /// <summary>
+    /// Outputs ranges representing a partitioned span
+    /// </summary>
+    /// <param name="span">The input span to partition</param>
+    /// <param name="chunks">Amount of chunks/partitions to create</param>
+    /// <typeparam name="T">Type of span</typeparam>
+    /// <returns>A new array containing all partition ranges</returns>
     public static Range[] Partition<T>(this ReadOnlySpan<T> span, int chunks)
     {
         var ranges = new Range[chunks];
@@ -33,12 +51,27 @@ public static class PartitionExtensions
         return ranges;
     }
     
+    /// <inheritdoc cref="Partition{T}(System.ReadOnlySpan{T},int)"/>
     public static Range[] Partition<T>(this Span<T> span, int chunks)
     => ((ReadOnlySpan<T>)span).Partition(chunks);
 
+    /// <summary>
+    /// Outputs ranges representing a partitioned array
+    /// </summary>
+    /// <param name="array">Array to partition</param>
+    /// <param name="chunks">Amount of chunks.partitions to create</param>
+    /// <param name="dst">Where to output the partition ranges</param>
+    /// <typeparam name="T">Type of array</typeparam>
     public static void Partition<T>(this T[] array, int chunks, Span<Range> dst)
         => array.AsSpan().Partition(chunks, dst);
 
+    /// <summary>
+    /// Outputs ranges representing a partitioned array
+    /// </summary>
+    /// <param name="array">Array to partition</param>
+    /// <param name="chunks">Amount of chunks.partitions to create</param>
+    /// <typeparam name="T">Type of array</typeparam>
+    /// <returns>A new array containing all partition ranges</returns>
     public static Range[] Partition<T>(this T[] array, int chunks)
         => array.AsSpan().Partition(chunks);
 }
--- Buffering/Parallel/PartitionParallelizer.cs
+++ Buffering/Parallel/PartitionParallelizer.cs
@@ -2,17 +2,47 @@
 
 namespace Buffering.Parallel;
 
+/// <summary>
+/// Used to partition an array of data and process it in parallel.
+/// All threads are new. All data is passed by reference from the array.
+///
+/// When chunk count and thread count differ, threads synchronize
+/// on an interlocked index variable. When they are the same, threads
+/// are initialized with their chunk only once with no synchronization.
+/// </summary>
+/// <typeparam name="T">Type of data</typeparam>
 public sealed class PartitionParallelizer<T>
 {
+    /// <summary>
+    /// Used to process some data on a thread
+    /// </summary>
     public delegate void ParallelRefDataHandler(ref T o);
     
+    /// <summary>
+    /// Amount of chunks for threads to work on.
+    /// The ideal chunk count equals thread count.
+    /// </summary>
     public int Chunks { get; }
+    /// <summary>
+    /// Amount of threads to work on chunks.
+    /// The ideal thread count equals chunk count.
+    /// </summary>
     public int ThreadCount { get; }
+    /// <summary>
+    /// Delegate to process the data on any thread.
+    /// </summary>
     public ParallelRefDataHandler DataHandler;
     private readonly Thread[] _threads;
     private readonly T[] _data;
     private readonly Range[] _chunkRanges;
 
+    /// <summary>
+    /// Initializes the PartitionParallelizer and the threads it will use based on the arguments.
+    /// </summary>
+    /// <param name="chunks">Amount of chunks</param>
+    /// <param name="threadCount">Amount of threads</param>
+    /// <param name="data">Data to partition/split into chunks</param>
+    /// <param name="dataHandler">Action to handle one datum by reference.</param>
     public PartitionParallelizer(int chunks, int threadCount, T[] data, ParallelRefDataHandler dataHandler)
     {
         Chunks = chunks;
@@ -81,6 +111,9 @@ public sealed class PartitionParallelizer<T>
             throw new ApplicationException("Threads not initialized");
     }
 
+    /// <summary>
+    /// Starts all threads and begins work on the partitioned data
+    /// </summary>
     public void Start()
     {
         EnsureThreadInit();
@@ -88,6 +121,9 @@ public sealed class PartitionParallelizer<T>
             t.Start();
     }
 
+    /// <summary>
+    /// Joins all threads, waiting for work to end on the partitioned data
+    /// </summary>
     public void Join()
     {
         EnsureThreadInit();
