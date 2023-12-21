5fcc4393026910de74768afac70892161996433b
Grovre <50428844+Grovre@users.noreply.github.com>
2023-12-20 21:27:59 -0600

    Implemented NoLock for non synchronized buffering

---
+++ Buffering/Locking/Locks/NoLock.cs
@@ -0,0 +1,35 @@
+﻿namespace Buffering.Locking.Locks;
+
+public class NoLock : IResourceLock
+{
+    public event EventHandler? Locking;
+    public event EventHandler? AfterLocked;
+    public event EventHandler? Unlocking;
+    public event EventHandler? AfterUnlocked;
+    
+    public ResourceLockHandle Lock(ResourceAccessFlags flags)
+    {
+        Locking?.Invoke(this, EventArgs.Empty);
+        AfterLocked?.Invoke(this, EventArgs.Empty);
+        return new ResourceLockHandle(this, ResourceAccessFlags.Generic);;
+    }
+
+    public bool TryLock(ResourceAccessFlags flags, out ResourceLockHandle hlock)
+    {
+        Locking?.Invoke(this, EventArgs.Empty);
+        hlock = new ResourceLockHandle(this, ResourceAccessFlags.Generic);
+        AfterLocked?.Invoke(this, EventArgs.Empty);
+        return true;
+    }
+
+    public void Unlock(ResourceLockHandle hlock)
+    {
+        Unlocking?.Invoke(this, EventArgs.Empty);
+        AfterUnlocked?.Invoke(this, EventArgs.Empty);
+    }
+
+    public IResourceLock Copy()
+    {
+        return new NoLock();
+    }
+}
